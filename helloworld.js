5e3420ecb9c0f53c882fd4cc76442d513e997556
Jayarath <jayarathpillu@gmail.com>
2021-05-06 17:02:20 +0530

    first commit

---
+++ helloworld.js
@@ -0,0 +1,43 @@
+// var str=`ada`;
+// var char="";
+// char=str[0];
+// console.log(char);
+/*var myname=5;
+myname=myname/2;
+console.log(myname);
+
+var ourarray=[1,2,3];
+ourarray.push(4);
+console.log(ourarray);
+ourarray.pop();
+console.log(ourarray);
+ourarray.shift();
+console.log(ourarray);
+function fun()
+{
+    console.log("my fun");
+}
+function fun2()
+{
+    myvar=5;
+}
+fun();
+fun();
+fun();
+console.log(myvar);
+*/
+function compareneq(a,b)
+{
+    if(a!=b)
+    {return true;}
+    return false;
+}
+if(compareneq(10,"10") && true)
+{
+    console.log("Not Equal");
+}
+else
+{
+    console.log("Equal"); 
+}
+
