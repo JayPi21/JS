d4b13cbe14088510b1f8e3e474719cf4b033b689
Jayarath <jayarathpillu@gmail.com>
2021-05-14 15:40:15 +0530

    fcc projects

--- helloworld.js
+++ helloworld.js
@@ -25,7 +25,7 @@ fun();
 fun();
 fun();
 console.log(myvar);
-*/
+
 function compareneq(a,b)
 {
     if(a!=b)
@@ -41,3 +41,47 @@ else
     console.log("Equal"); 
 }
 
+var recordCollection = {
+    2548: {
+      albumTitle: 'Slippery When Wet',
+      artist: 'Bon Jovi',
+      tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    }
+};
+console.log(recordCollection[2548].albumTitle);
+
+"use strict";
+let hello;
+//hello="my";
+console.log(hello);
+
+var hell=5;
+console.log(hell + " songs");
+
+var myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
+    }
+  };
+  
+  var gloveBoxContents = myStorage.car.inside["glove box"];
+  console.log(gloveBoxContents);
+  */
+  // Setup
+  
+  //console.log(rangeOfNumbers(3,9));
+  function fun(...args)
+  {
+    for(var i=0;i<args.length;i++)
+    {console.log(args[i]);
+    }
+  }
+//fun(1,2,3,4,5,6,7,8,9);
+var arr=[1,2,3,4,5];
+fun(...arr);
---
+++ new.js
@@ -0,0 +1,28 @@
+// Only change code below this line
+/*
+class Thermostat{
+    constructor(temp)
+     {
+       this.temp=5/9 * (temp-32);
+     }
+     get temperature()
+     {return this.temp;}
+     set temperature(temperature)
+     { this.temp=temperature;
+  }
+  }
+  // Only change code above this line
+  
+  const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+  let temp = thermos.temperature; // 24.44 in Celsius
+  thermos.temperature = 26;
+  temp = thermos.temperature;
+  console.log(thermos); // 26 in Celsius
+  
+  var string = 
+  `hello world!`;
+  console.log(string);
+  */
+ let arr=[1,2,4];
+ arr=[4,5,6];
+ console.log(arr);
